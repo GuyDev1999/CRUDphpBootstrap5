97808dfbb6e4e3964114567fc1a447923c982713
Jetguy <lawbreakser@gmail.com>
2022-01-24 15:31:24 +0700

    add bootstrap5

--- index.php
+++ index.php
@@ -9,7 +9,48 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    
+
+    <div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+        <div class="modal-body">
+            <form action="insert.php" method="post" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label for="firstname" class="col-form-label">First Name:</label>
+                    <input type="text" required class="form-control" name="firstname">
+                </div>
+                <div class="mb-3">
+                    <label for="lastname" class="col-form-label">Last Name:</label>
+                    <input type="text" required class="form-control" name="lastname">
+                </div>
+                <div class="mb-3">
+                    <label for="lastname" class="col-form-label">Last Name:</label>
+                    <input type="text" required class="form-control" name="lastname">
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Send message</button>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>CRUD Bootstrap</h1>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#userModal">Add User</button>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
