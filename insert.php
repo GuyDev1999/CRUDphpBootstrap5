3c28853744f6ea055423e99a460508ebbc9dc049
Jetguy <lawbreakser@gmail.com>
2022-01-24 16:50:42 +0700

    Insert data and check file img

--- insert.php
+++ insert.php
@@ -15,4 +15,26 @@
         $fileNew = rand() . "." . $fileActExt; // Random ชื่อ file 
         $filePath = "uploads/".$fileNew;
 
+        if (in_array($fileActExt, $allow)) {
+            if ($img['size'] > 0 && $img['error'] == 0) {
+                if (move_uploaded_file($img['tmp_name'], $filePath)) {
+                    $sql = $conn->prepare("INSERT INTO users(firstname, lastname, position, img)
+                            VALUES(:firstname, :lastname, :position, :img)");
+                    $sql->bindParam(":firstname", $firstname);
+                    $sql->bindParam(":lastname", $lastname);
+                    $sql->bindParam(":position", $position);
+                    $sql->bindParam(":img", $fileNew);
+                    $sql->execute();
+
+                    if ($sql) {
+                        $_SESSION['success'] = "Data has been inserted successfully";
+                        header("location: index.php");
+                    } else {
+                        $_SESSION['error'] = "Data inserted failed";
+                        header("location: index.php");
+                    }
+                }
+            }
+        }
+
     }
