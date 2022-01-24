d9f5d31a7e705c15e1cfa6d070ad309bbc25571d
Jetguy <lawbreakser@gmail.com>
2022-01-24 18:21:05 +0700

    add value form edit and style

--- edit.php
+++ edit.php
@@ -12,6 +12,12 @@ require_once 'config/db.php';
     <title>CRUD PDO & Bootstrap 5</title>
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+        .container {
+            max-width: 550px;
+        }
+    </style>
 </head>
 <body>
 
@@ -30,20 +36,20 @@ require_once 'config/db.php';
             ?>
             <div class="mb-3">
                 <label for="firstname" class="col-form-label">First Name:</label>
-                <input type="text" required class="form-control" name="firstname">
+                <input type="text" value="<?= $data['firstname']; ?>" required class="form-control" name="firstname">
             </div>
             <div class="mb-3">
                 <label for="lastname" class="col-form-label">Last Name:</label>
-                <input type="text" required class="form-control" name="lastname">
+                <input type="text" value="<?= $data['lastname']; ?>" required class="form-control" name="lastname">
             </div>
             <div class="mb-3">
                 <label for="position" class="col-form-label">Position:</label>
-                <input type="text" required class="form-control" name="position">
+                <input type="text" value="<?= $data['position']; ?>" required class="form-control" name="position">
             </div>
             <div class="mb-3">
                 <label for="img" class="col-form-label">Image:</label>
                 <input type="file" required class="form-control" id="imgInput" name="img">
-                <img width="100%" id="previewImg" alt="">
+                <img width="100%" src="uploads/<?=$data['img']; ?>" id="previewImg" alt="">
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
